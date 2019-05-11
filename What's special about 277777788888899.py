e2bb2ffa7fd722ddd9542d8193335ae1d9b0e1a6
zperk13 <zperk13>
2019-05-11 11:33:39 -0500

    added random versions of find_multiplication_persistence

--- What's special about 277777788888899.py
+++ What's special about 277777788888899.py
@@ -1,4 +1,5 @@
 # https://youtu.be/Wim9WJeDTHQ
+from random import randint
 def multiplication_persistence(x, print_steps=False):
     def multiply_digits(y):
         result = 1
@@ -42,9 +43,36 @@ def find_multiplication_persistence(max_num, print_if_equal=False, start_num=1):
                 best = result
             num += 1
 
+def random_find_multiplication_persistence(min_num, max_num):
+    num = min_num
+    best = 1
+    while num < max_num:
+        result = multiplication_persistence(num)
+        if result > best:
+            print(f'Number found: {num:,} with {result} steps')
+            best = result
+        num = randint(min_num, max_num)
+
+def smart_random_find_multiplication_persistence():
+    best = 1
+    while True:
+        num_list = ['2', '6', '7', '8', '9']
+        string = ''
+        for _ in range(randint(232, 250)):
+            index = randint(0, len(num_list)-1)
+            string+=num_list[index]
+            num_list = num_list[index:]
+        number = int(string)
+        result = multiplication_persistence(number)
+        if result > best:
+            print(f'Number found: {number:,} with {result} steps')
+            best = result
 
 if __name__ == '__main__':
     title_number = 277_777_788_888_899
     # print(multiplication_persistence(title_number))
-    find_multiplication_persistence(title_number+1)
+    # find_multiplication_persistence(title_number+1)
     # find_multiplication_persistence(int('1'+'0'*250), start_num=int('1'+'0'*232))
+    # randint(int('1' + '0' * 232), int('1' + '0' * 250))
+    # random_find_multiplication_persistence(int('1' + '0' * 232), int('1' + '0' * 250))
+    smart_random_find_multiplication_persistence()
