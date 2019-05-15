ff369f4ea9fbab6bda14d61868c873efb7c90223
zperk13 <zperk13>
2019-05-14 21:57:45 -0500

    The Graceful Tree Problem

---
+++ The Graceful Tree Problem.py
@@ -0,0 +1,32 @@
+def difference(num1, num2):
+    return max([num1, num2]) - min([num1, num2])
+
+
+def get_ant_numbers(antenna1, antenna2, head, thorax, abdomen):
+    numbers = []
+    numbers.append(difference(antenna1, head))
+    numbers.append(difference(antenna2, head))
+    numbers.append(difference(head, thorax))
+    numbers.append(difference(thorax, abdomen))
+    return numbers
+
+
+if __name__ == '__main__':
+    numbers = [1, 3, 5, 7, 9]
+    count = 0
+    for a1 in numbers:
+        for a2 in [num for num in numbers if num not in [a1]]:
+            for h in [num for num in numbers if num not in [a1, a2]]:
+                for t in [num for num in numbers if num not in [a1, a2, h]]:
+                    ab = [num for num in numbers if num not in [a1, a2, h, t]][0]
+                    ant_numbers = get_ant_numbers(a1, a2, h, t, ab)
+                    checked = []
+                    for num in ant_numbers:
+                        if num in checked:
+                            break
+                        else:
+                            checked.append(num)
+                    if len(checked) == 4:
+                        count += 1
+                        print(a1, a2, h, t, ab, checked)
+    print(count)
